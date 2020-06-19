dbd095ba7a6aedea4aeddf5b6fb8e557ae0e3ccb
Wanjiru Catherine <catherinemunene802@gmail.com>
2020-06-19 11:58:47 +0300

    Design Completed

--- quick_think/lib/main.dart
+++ quick_think/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
--- quick_think/lib/splashpage/splashpage.dart
+++ quick_think/lib/splashpage/splashpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -8,11 +9,61 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      //Setting background color
-      backgroundColor: Color(0xFF524971),
-      body: Center(
-        child: Image.asset('assets/AppName.png'),
+      //Background color
+      backgroundColor: Color(0xFF1C1046),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          _buildVector(height, width),
+          _buildAppName(),
+        ],
+      ),
+    );
+  }
+
+//Vector Image
+  Widget _buildVector(height, width) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/splash_vector.png',
+          ),
+          //   fit: BoxFit.scaleDown
+        ),
+      ),
+    );
+  }
+
+  /// AppName
+  Widget _buildAppName() {
+    return Center(
+      child: Container(
+        child: RichText(
+          text: TextSpan(
+            text: 'Quick',
+            style: GoogleFonts.dmSans(
+              fontSize: 32,
+              color: Color(0xFFFFFFFF),
+              fontWeight: FontWeight.bold,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Think',
+                style: GoogleFonts.dmSans(
+                  fontSize: 32,
+                  color: Color(0xFF18C5D9),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
