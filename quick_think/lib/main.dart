9684dccf3d25399c0a7a95a754ea3950f17c5f89
Josteve <adekanbijosteve@gmail.com>
2020-06-20 21:40:36 +0100

    select difficulty bottom sheet done

--- quick_think/lib/main.dart
+++ quick_think/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quickthink/select_difficulty_utils.dart';
 
 void main() {
   runApp(MyApp());
@@ -108,7 +109,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: (){
+          showDifficultyBottomSheet(context);
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
