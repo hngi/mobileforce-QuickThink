81d8d94d34784cb3824c06cf4d23a451e10788d6
OlaleyeJumoke <olaleyesilver@gmail.com>
2020-06-22 00:49:24 -1100

    Screen folder updated

--- quick_think/lib/screens/registration.dart
+++ quick_think/lib/screens/registration.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:quickthink/image.dart';
 
 import 'bottom_navigation_bar.dart';
 
