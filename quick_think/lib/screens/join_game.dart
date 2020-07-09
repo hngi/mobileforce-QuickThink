337acb5185c8deb2fa665f61cb6af8638d76c37f
Wanjiru Catherine <catherinemunene802@gmail.com>
2020-07-09 12:01:00 +0300

    Working on making screens responsive

---
+++ quick_think/lib/screens/join_game.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:quickthink/utils/responsiveness.dart';
+
+class JoinGame extends StatefulWidget {
+  @override
+  _JoinGameState createState() => _JoinGameState();
+}
+
+class _JoinGameState extends State<JoinGame> {
+  SizeConfig _sizeConfig;
+
+  String nick = '';
+  String password = '';
+
+  final _formKey = GlobalKey<FormState>();
+
+  ProgressDialog progressDialog;
+
+  @override
+  Widget build(BuildContext context) {
+    progressDialog = new ProgressDialog(context,
+        isDismissible: false, type: ProgressDialogType.Normal);
+
+    progressDialog.style(
+      borderRadius: 10.0,
+      backgroundColor: Colors.white,
+      progressWidget: SpinKitThreeBounce(color: Color(0xFF18C5D9), size: 25),
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeOut,
+      progressTextStyle: TextStyle(
+          color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
+      messageTextStyle: TextStyle(
+          color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
+    );
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _logoText(),
+              SizedBox(
+                height: SizeConfig().yMargin(context, 10),
+              ),
+              _prompt(),
+               SizedBox(
+                height: SizeConfig().yMargin(context, 3.0),
+              ),
+              _quizCode(),
+              SizedBox(
+                height: SizeConfig().yMargin(context, 4),
+              ),
+              _username(),
+              SizedBox(
+                height: SizeConfig().yMargin(context, 7),
+              ),
+              _loginBtn(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _prompt() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: SizeConfig().xMargin(context, 5.0),
+        right: SizeConfig().xMargin(context, 3.0),
+      ),
+      child: Text(
+        'Emeka invited you to join the game',
+        style: TextStyle(
+          fontFamily: 'Poppins',
+          color: Colors.white,
+          fontWeight: FontWeight.w600,
+          fontSize: SizeConfig().textSize(context, 3.7),
+        ),
+      ),
+    );
+  }
+
+  Widget _logoText() {
+    return RichText(
+      text: TextSpan(children: <TextSpan>[
+        TextSpan(
+            text: 'Quick',
+            style: TextStyle(
+                fontFamily: 'DM Sans',
+                fontWeight: FontWeight.w700,
+                fontSize: SizeConfig().textSize(context, 3),
+                color: Colors.white)),
+        TextSpan(
+            text: 'Think',
+            style: TextStyle(
+              fontFamily: 'DM Sans',
+              fontWeight: FontWeight.w700,
+              fontSize: SizeConfig().textSize(context, 3),
+              color: Color.fromRGBO(24, 197, 217, 1),
+            ))
+      ]),
+    );
+  }
+
+  Widget _username() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: SizeConfig().xMargin(context, 5.0),
+        right: SizeConfig().xMargin(context, 3.0),
+      ),
+      child: TextFormField(
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            fontSize: SizeConfig().textSize(context, 3),
+            color: Colors.white),
+        onChanged: (val) {},
+        validator: (val) {
+          if (val.length == 0) {
+            return 'Username Should Not Be Empty';
+          }
+          if (val.length <= 2) {
+            return 'should be 3 or more characters';
+          }
+          if (!RegExp(r"^[a-z0-9A-Z_-]{3,16}$").hasMatch(val)) {
+            return "can only include _ or -";
+          }
+          return null;
+        },
+        onSaved: (val) => nick = val,
+        decoration: InputDecoration(
+          hintText: 'Enter Username',
+          hintStyle: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w400,
+              fontSize: 20.0,
+              color: Colors.white),
+          contentPadding: EdgeInsets.fromLTRB(14.0, 12.0, 0.0, 12.0),
+          fillColor: Color.fromRGBO(87, 78, 118, 1),
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: Color.fromRGBO(24, 197, 217, 1), width: 1.0),
+              borderRadius: BorderRadius.circular(5.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _quizCode() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: SizeConfig().xMargin(context, 5.0),
+        right: SizeConfig().xMargin(context, 3.0),
+      ),
+      child: TextFormField(
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            fontSize: SizeConfig().textSize(context, 3),
+            color: Colors.white),
+        onChanged: (val) {},
+        validator: (val) {
+          if (val.length == 0) {
+            return 'Username Should Not Be Empty';
+          }
+          if (val.length <= 2) {
+            return 'should be 3 or more characters';
+          }
+          if (!RegExp(r"^[a-z0-9A-Z_-]{3,16}$").hasMatch(val)) {
+            return "can only include _ or -";
+          }
+          return null;
+        },
+        onSaved: (val) => nick = val,
+        decoration: InputDecoration(
+          hintText: 'Enter Game Code',
+          hintStyle: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w400,
+              fontSize: 20.0,
+              color: Colors.white),
+          contentPadding: EdgeInsets.fromLTRB(14.0, 12.0, 0.0, 12.0),
+          fillColor: Color.fromRGBO(87, 78, 118, 1),
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: Color.fromRGBO(24, 197, 217, 1), width: 1.0),
+              borderRadius: BorderRadius.circular(5.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginBtn() {
+    return RaisedButton(
+      padding: EdgeInsets.fromLTRB(70, 20, 70, 20),
+      onPressed: () {
+        onPressed();
+      },
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      color: Color.fromRGBO(24, 197, 217, 1),
+      highlightColor: Color.fromRGBO(24, 197, 217, 1),
+      child: Text('Get started',
+          style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w700,
+              fontSize: 16.0,
+              color: Colors.white)),
+    );
+  }
+
+  void onPressed() async {
+    var form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      //    handleRegistration(nick, password);
+    }
+  }
+}
