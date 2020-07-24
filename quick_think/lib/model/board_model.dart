61b46a9bd9bddf13524e30deb0ed61f9524d32bf
KingOsborne <stanleeadigwe@yahoo.com>
2020-07-24 10:03:00 +0100

    Leaderboard version two

---
+++ quick_think/lib/model/board_model.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+enum fetchState {Busy, DataRetrieved,NoData}
+
+class BoardModel{
+  final StreamController<fetchState> _playedStateController = StreamController<fetchState>.broadcast();
+  final StreamController<fetchState> _createdStateController = StreamController<fetchState>.broadcast();
+
+  Stream<fetchState> get playedState => _playedStateController.stream;
+  Stream<fetchState> get createdState => _createdStateController.stream;
+
+  List<String> playedGames;
+  List<String> createdGames;
+
+  Future<List<String>> getCreatedPrefs(String key) async {
+
+    _createdStateController.add(fetchState.Busy);
+
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final valueStored = sharedPreferences.getStringList(key) ?? [];
+    print('List Retrieved with Play Game Codes in Join Game: $valueStored');
+
+    if(valueStored != null || valueStored.length != 0){
+      createdGames = valueStored;
+      _createdStateController.add(fetchState.DataRetrieved);
+    }else{
+      _createdStateController.add(fetchState.NoData);
+    }
+  }
+
+  Future<List<String>> getPlayedPrefs(String key) async {
+
+    _playedStateController.add(fetchState.Busy);
+
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final valueStored = sharedPreferences.getStringList(key) ?? [];
+    print('List Retrieved with Play Game Codes in Join Game: $valueStored');
+
+    if(valueStored != null || valueStored.length != 0){
+      playedGames = valueStored;
+      _playedStateController.add(fetchState.DataRetrieved);
+    }else{
+      _playedStateController.add(fetchState.NoData);
+    }
+  }
+}
