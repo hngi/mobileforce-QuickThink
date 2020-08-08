3107f1850e516e9be2aaf1a2f6ec461221edfe6b
KingOsborne <stanleeadigwe@yahoo.com>
2020-08-08 11:05:34 +0100

    Leaderboard bug fix

--- quick_think/lib/model/leaderboard_model.dart
+++ quick_think/lib/model/leaderboard_model.dart
@@ -16,7 +16,8 @@ Future<List<LeaderboardList>> getLeaderboard(String gameCode) async {
 
   _stateController.add(fetchState.Busy);
 
-  final String fetchUrl = "https://brainteaser.pythonanywhere.com/game/code/leaderboard?n=100&game_code=$gameCode";
+  final String fetchUrl = "https://brainteaserdev.pythonanywhere.com//usergame/leaderboard/game/$gameCode/100/";
+
   Response data = await get(fetchUrl);
 
   if(data.statusCode == 200){
